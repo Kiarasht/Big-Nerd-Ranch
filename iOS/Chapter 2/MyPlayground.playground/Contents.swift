059cc82aed6e083a44497392d91bc38a5bca0f2a
Kiarasht <torkian@pdx.edu>
2017-09-02 12:04:37 -0700

    Playground

---
+++ iOS/Chapter 2/MyPlayground.playground/Contents.swift
@@ -0,0 +1,46 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+str = "Hello, Swift"
+let constantStr = str
+
+var nextYear: Int
+var bodyTemp: Float
+var hasPet: Bool
+var arrayOfInts: [Int]
+var dictionaryOfCapitalsByCountry: [String:String]
+var winningLotteryNumbers: Set<Int>
+
+let number = 42
+let fmStation = 91.1
+
+var countingUp = ["one", "two"]
+countingUp.count
+let nameByParkingSpace = [13: "Alice", 27: "Bob"]
+let secondElement = countingUp[1]
+countingUp.append("three")
+countingUp.count
+
+let emptyString = String()
+emptyString.isEmpty
+let nonEmptyString = String(number)
+nonEmptyString.isEmpty
+let emptyArrayOfInts = [Int]()
+let emptySetOfFloats = Set<Float>()
+
+let defaultNumber = Int()
+let defaultBool = Bool()
+
+let allOfLifeAnswers = 42;
+let allOfLifeAnswersInString = String(allOfLifeAnswers)
+
+let availableRooms = Set([205, 411, 412])
+
+let defaultFloat = Float()
+let floatFromLiteral = Float(3.14)
+
+let easyPi = 3.14
+let floatFromDouble = Float(easyPi)
+let floatingPi: Float = 3.14
